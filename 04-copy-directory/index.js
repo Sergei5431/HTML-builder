be4e6fcbabc0296967f9ea280ecba4dd4b10e15f
sergei5431-JSFEPRESCHOOL <ykishenii@gmail.com>
2022-05-23 11:14:11 +0300

    feat: task fuor

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
 
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'fils'), (error, data)=>{
+  if(error) console.log(error);
+  fs.mkdir(path.join(__dirname, 'files-copy'), ()=>{
+    data.forEach(e=>{
+      fs.copyFile(path.join(__dirname, `files/${e}`), path.join(__dirname, `files-copy/${e}`), ()=>{} );
+    });
+  });
+});
